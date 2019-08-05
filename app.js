45404f8a37223f501a809d5636908e747d148e8b
Alice <alice.leon10@gmail.com>
2019-08-05 10:00:10 +0200

    Ajout de CORS pour pouvoir faire des requêtes à partir du client :)

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 //Import d'express et d'autres dépendances nécessaires au fonctionnement de l'application.
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
+//Configuration de CORS pour accepter les requêtes provenant du client
+/* const corsOptions = {
+  //Définir l'adresse du client ici :
+  origin: 'http://localhost:8000',
+  optionsSuccessStatus: 200,
+}; */
+
+//Utilisation du middleware cors pour pouvoir faire des requêtes à partir du client.
+app.use(cors());
 //Utilisation du middleware bodyParser pour pouvoir traiter les requêtes.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
