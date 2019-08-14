097234ca3fe1bc046e718db8ab360ffcb75ac207
Alice <alice.leon10@gmail.com>
2019-08-14 17:08:24 +0200

    Re-changement des données de connexion à la base de données

--- db.js
+++ db.js
@@ -11,13 +11,22 @@ const mysql = require('mysql');
 }); */
 
 //Configuration pour déploiement sur Clever Cloud
-const pool = mysql.createPool({
+/* const pool = mysql.createPool({
   connectionLimit: 10,
   host: 'b53hlf0zjrgkbo7m2fik-mysql.services.clever-cloud.com',
   port: '3306',
   user: 'uyvzyidwyq1erced',
   password: 'FOaPjHnrsotgfzpCOal6',
   database: 'b53hlf0zjrgkbo7m2fik',
+}); */
+
+//Configuration pour deploiement, bis :
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.MYSQL_ADDON_HOST,
+  database: process.env.MYSQL_ADDON_DB,
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
 });
 
 module.exports = pool;
