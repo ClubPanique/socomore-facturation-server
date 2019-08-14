bd601490eb25431f8095b5f5b41305fb050b3906
Alice <alice.leon10@gmail.com>
2019-08-14 16:52:07 +0200

    Changement des paramètres de connexion à la base de données

--- db.js
+++ db.js
@@ -1,12 +1,23 @@
 const mysql = require('mysql');
 
-const pool = mysql.createPool({
+//Configuration pour connexion en local
+/* const pool = mysql.createPool({
   connectionLimit: 10,
   host: 'localhost',
   port: '3306',
   user: 'root',
   password: '',
   database: 'socomore-facturation',
+}); */
+
+//Configuration pour déploiement sur Clever Cloud
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'b53hlf0zjrgkbo7m2fik-mysql.services.clever-cloud.com',
+  port: '3306',
+  user: 'uyvzyidwyq1erced',
+  password: 'FOaPjHnrsotgfzpCOal6',
+  database: 'b53hlf0zjrgkbo7m2fik',
 });
 
 module.exports = pool;
